3dc27bda5c7ee0eb5ecf71d798552ab038dca46c
Victor <victorbyhlin1@gmail.com>
2020-03-08 00:17:07 +0100

    added some js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,18 @@
 import '../styles/styles.css'
+import Person from './modules/Person'
 
 if (module.hot) {
   module.hot.accept()
 }
+
+class Adult extends Person {
+  payTaxes(){
+    console.log(this.name + " betalar skatt");
+  }
+}
+
+var John = new Person("John Doe", "Yellow");
+John.greet();
+
+var Greta = new Adult("Greta Thunberg", "Green");
+Greta.payTaxes();
