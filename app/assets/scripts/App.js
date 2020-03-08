4c6ae26d763c69cdde100a432a54ef2244c7f0bb
Victor <victorbyhlin1@gmail.com>
2020-03-08 23:33:01 +0100

    Fixed interactive menu button

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,8 @@
-import '../styles/styles.css'
-import Person from './modules/Person'
+import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu.js';
 
 if (module.hot) {
   module.hot.accept()
 }
 
-class Adult extends Person {
-  payTaxes(){
-    console.log(this.name + " betalar skatt");
-  }
-}
-
-var John = new Person("John Doe", "Yellow");
-John.greet();
-
-var Greta = new Adult("Greta Thunberg", "Green");
-Greta.payTaxes();
+let mobileMenu = new MobileMenu();
